4e657aafb5b5080284ff860d832f7fa2b9a03731
aringlis <ainglis@gs671-ainglis.ndc.nasa.gov>
2020-01-27 15:19:43 -0500

    tidy up code and clean up imports

--- afino_main_analysis3.py
+++ afino_main_analysis3.py
@@ -7,11 +7,9 @@ import numpy as np
 import copy
 import os
 import sys
-from matplotlib import pyplot as plt
 from timeseries import TimeSeries
 import rnspectralmodels3
 import afino_model_fitting
-import pickle
 
 def main_analysis(ts, model='single_power_law_with_constant', low_frequency_cutoff=None,
                       overwrite_gauss_bounds = None, overwrite_extra_gauss_bounds = None):
@@ -90,19 +88,16 @@ def main_analysis(ts, model='single_power_law_with_constant', low_frequency_cuto
     if model == 'single_power_law_with_constant':
         jack_bic = afino_model_fitting.BIC(2,best_param_vals,frequencies,iobs,rnspectralmodels3.power_law_with_constant,len(iobs))
         best_fit_power_spectrum = rnspectralmodels3.power_law_with_constant(best_param_vals,frequencies)
-        best_fit_params = best_param_vals
     elif model == 'splwc_AddNormalBump2':
         jack_bic = afino_model_fitting.BIC(5,best_param_vals,frequencies,iobs,rnspectralmodels3.splwc_AddNormalBump2,len(iobs))
         best_fit_power_spectrum = rnspectralmodels3.splwc_AddNormalBump2(best_param_vals,frequencies)
-        best_fit_params = best_param_vals
     elif model == 'broken_power_law_with_constant':
         jack_bic = afino_model_fitting.BIC(4,best_param_vals,frequencies,iobs,rnspectralmodels3.broken_power_law_with_constant,len(iobs))
         best_fit_power_spectrum = rnspectralmodels3.broken_power_law_with_constant(best_param_vals,frequencies)
-        best_fit_params = best_param_vals
     elif model == 'splwc_AddNormalBump2_plus_extra_bump':
         jack_bic = afino_model_fitting.BIC(8,best_param_vals,frequencies,iobs,rnspectralmodels3.splwc_AddNormalBump2_plus_extra_bump,len(iobs))
         best_fit_power_spectrum = rnspectralmodels3.splwc_AddNormalBump2_plus_extra_bump(best_param_vals,frequencies)
-        best_fit_params = best_param_vals
+    best_fit_params = best_param_vals
 
 
     #----------------------------------------
@@ -187,36 +182,4 @@ def randomize_initial_guess(model = 'single_power_law_with_constant'):
         
     return initial_guess
     
-
-#def randomize_initial_guess_plus_extra_bump():
-    
-    #randomize width between 0.05 and 0.25
- #   gauss_width = (np.random.random(1) / 5.) + 0.05
- #   gauss_position = np.random.random(1) * (-4) - 1.5
-  #  gauss_amp = np.random.random(1) * (-14)
-  #  plaw_amp = (np.random.random(1) *20) - 10
-  #  plaw_index = np.random.random(1) * (-5) - 1
-  #  background = np.random.random(1) * (-20)
-    
-  #  extra_gauss_amp = np.random.random(1) * (-14)
-  #  extra_gauss_position = (np.random.random(1) / 5.) -3.1
-#    extra_gauss_width = (np.random.random(1) / 10.0) + 0.05
-    
-  #  initial_guess = [plaw_amp[0], plaw_index[0], background[0], gauss_amp[0], gauss_position[0], gauss_width[0], extra_gauss_amp[0], extra_gauss_position[0], extra_gauss_width[0]]
-
-  #  return initial_guess
-
-
-#def randomize_initial_guess_bpow():
- #    
-  #  plaw_amp = (np.random.random(1) *20) - 10
-   # plaw_index = np.random.random(1) * (-5) - 1
-   # plaw_index2 = np.random.random(1) * (-5) - 1
-   # break_position = np.random.random(1) * (-4) - 1.5
-   # background = np.random.random(1) * (-20)
-
-   # initial_guess = [plaw_amp[0], plaw_index[0], np.exp(break_position[0]), plaw_index[0], background[0]]
-
-   # return initial_guess
-
 
