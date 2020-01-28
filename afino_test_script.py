c73a293bf798d9c6a727fdd22cbcf02b288717c5
aringlis <ainglis@gs671-ainglis.ndc.nasa.gov>
2020-01-28 13:28:16 -0500

    changed analyse_generic_series to afino_start

--- afino_test_script.py
+++ afino_test_script.py
@@ -1,5 +1,5 @@
 
-import analyse_generic_series
+import afino_start
 from astropy.io import fits
 
 result = fits.open('test_data/flare566801.fits')
@@ -7,6 +7,6 @@ hdu1 = result[1]
 flux = hdu1.data['flux']
 tt = hdu1.data['time']
 tt2 = tt*60
-analyse_generic_series.analyse_series(tt2,flux,low_frequency_cutoff=10,
+afino_start.analyse_series(tt2,flux,low_frequency_cutoff=10,
                                       overwrite_gauss_bounds = [(-10.0,10.0),(-1.0,6.0),
                                                                 (-20.0,10.0),(-16.0,5.0),(-9.0,-5.0),(0.05,0.25)]) 
