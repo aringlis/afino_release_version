6b4c8610e963ed90c5cb33f3f9b79428a4c38d2b
aringlis <ainglis@gs671-ainglis.fios-router.home>
2020-04-03 15:54:47 -0400

    added another unit test

--- tests/test_afino.py
+++ tests/test_afino.py
@@ -3,6 +3,17 @@ import numpy as np
 from afino import afino_series
 
 
+def test_afinoseries():
+    tt = np.linspace(0,100,101)
+    flux = np.random.random(101)
+    ts = afino_series.AfinoSeries(tt,flux)
+
+    assert ts.SampleTimes.nt == 101
+    assert ts.SampleTimes.dt == 1.0
+    assert len(ts.data) == 101
+    assert len(ts.PowerSpectrum.frequencies.positive) == 50
+    assert len(ts.PowerSpectrum.ppower) == 50
+
 def test_prep_series():
     tt = np.linspace(0,100,101)
     flux = np.random.random(101)
