e78dd19757509f6e8085f84faf65f715a54cbe91
aringlis <ainglis@gs671-ainglis.fios-router.home>
2020-04-06 15:33:50 -0400

    unit test for power law model

--- tests/test_afino.py
+++ tests/test_afino.py
@@ -1,7 +1,9 @@
 # tests for AFINO
 import numpy as np
+from numpy.testing import assert_almost_equal
 from afino import afino_series
 from afino import afino_utils
+from afino import afin_spectral_models
 
 
 def test_afinoseries():
@@ -34,6 +36,14 @@ def test_nothing():
     pass
 
 
+# various tests for the model functions
+
+def test_pow():
+    freqs = np.linspace(0.001,0.5)
+    power = afino_spectral_models.pow([1.0,1.0],f)
+    assert_almost_equal(power[0], 2.718, decimal = 3)
+
+    
 
 
 
